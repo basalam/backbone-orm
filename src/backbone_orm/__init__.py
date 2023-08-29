d8eb589e5d86743a5cbff7f75203d8598403a97c
m.habibian <mojtabaa.hn@gmail.com>
2023-08-29 15:07:00 +0330

    feat: pagination

--- src/backbone_orm/__init__.py
+++ src/backbone_orm/__init__.py
@@ -3,6 +3,7 @@ from .factory_abstract import FactoryAbstract, U
 from .migration_abstract import MigrationAbstract
 from .model_abstract import ModelAbstract, T
 from .model_schema_abstract import ModelSchemaAbstract
+from .pagination import PaginationResponse
 from .parameters import Parameters
 from .postgres_connection import PostgresConnection
 from .postgres_transaction import PostgresTransaction
