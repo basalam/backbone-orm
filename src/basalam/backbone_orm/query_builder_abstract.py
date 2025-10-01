357f951e589bbd4f2ff4d97ee136c32f83644907
m.kooti <mostafa.kooti@basalam.com>
2025-10-01 12:28:11 +0330

    Updates the less than or equal to operator to 'lte' for consistency.

--- src/basalam/backbone_orm/query_builder_abstract.py
+++ src/basalam/backbone_orm/query_builder_abstract.py
@@ -25,7 +25,7 @@ class Filters:
     GREATER_THAN = 'gt'
     GREATER_THAN_OR_EQUAL = 'gte'
     LESS_THAN = 'lt'
-    LESS_THAN_OR_EQUAL = 'le'
+    LESS_THAN_OR_EQUAL = 'lte'
     CONTAINS = 'contains'
     ICONTAINS = 'icontains'
     IN = 'in'
